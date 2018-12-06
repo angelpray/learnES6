39db514497cf2c7ff114548759d196cd96f520b2
angelpray <1136472002@qq.com>
2018-12-06 15:35:18 +0800

    finish feature-module

---
+++ module/index.js
@@ -0,0 +1,11 @@
+// import {A, test, Hello} from "./lesson-18";
+// import * as lesson from "./lesson-18";
+
+// console.log(lesson.A);
+// lesson.test();
+// lesson.Hello.test();
+
+import Lesson18 from "./lesson-18";
+
+console.log(Lesson18.A);
+console.log(Lesson18.test);
